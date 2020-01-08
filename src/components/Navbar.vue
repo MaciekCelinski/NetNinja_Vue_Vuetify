8391a258e9db6f509037ed35af365f49a01d2573
MaciekCelinski <mac.celinski@gmail.com>
2020-01-08 11:17:21 +0100

    lesson 25 - Datepickers

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,76 +1,105 @@
 <template>
-  <nav>
-    <v-toolbar flat>
-      <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title class="text-uppercase grey--text">
-        <span class="font-weight-light">TODO</span>
-        <span>Ninja</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
+	<nav>
+		<v-toolbar flat>
+			<v-app-bar-nav-icon
+				class="grey--text"
+				@click="drawer = !drawer"
+			></v-app-bar-nav-icon>
+			<v-toolbar-title class="text-uppercase grey--text">
+				<span class="font-weight-light">TODO</span>
+				<span>Ninja</span>
+			</v-toolbar-title>
+			<v-spacer></v-spacer>
 
-      <template>
-        <div class="text-center">
-          <v-menu offset-y>
-            <template v-slot:activator="{ on }">
-              <v-btn dark v-on="on" text color="grey">
-                <v-icon text>mdi-chevron-down</v-icon>Menu
-              </v-btn>
-            </template>
-            <v-list>
-              <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
-                <v-list-item-title>{{ link.text }}</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
-        </div>
-      </template>
+			<template>
+				<div class="text-center">
+					<v-menu offset-y>
+						<template v-slot:activator="{ on }">
+							<v-btn dark v-on="on" text color="grey">
+								<v-icon text>mdi-chevron-down</v-icon>Menu
+							</v-btn>
+						</template>
+						<v-list>
+							<v-list-item
+								v-for="link in links"
+								:key="link.text"
+								router
+								:to="link.route"
+							>
+								<v-list-item-title>{{ link.text }}</v-list-item-title>
+							</v-list-item>
+						</v-list>
+					</v-menu>
+				</div>
+			</template>
 
-      <v-btn text color="grey">
-        <span>Sign Out</span>
-        <v-icon right>mdi-exit-to-app</v-icon>
-      </v-btn>
-    </v-toolbar>
+			<v-btn text color="grey">
+				<span>Sign Out</span>
+				<v-icon right>mdi-exit-to-app</v-icon>
+			</v-btn>
+		</v-toolbar>
 
-    <v-navigation-drawer v-model="drawer" app temporary absolute class="primary">
-      <v-layout column align-center>
-        <v-flex class="mt-5">
-          <v-avatar size="85">
-            <v-img src="/avatar-1.png"></v-img>
-          </v-avatar>
-          <p class="white--text subheading mt-1">The Net Ninja</p>
-        </v-flex>
-      </v-layout>
-      <v-list>
-        <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
-          <v-list-item-action>
-            <v-icon class="white--text">{{ link.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title id="link-active" class="white--text">{{ link.text }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-  </nav>
+		<v-navigation-drawer
+			v-model="drawer"
+			app
+			temporary
+			absolute
+			class="purple accent-4"
+		>
+			<v-layout column align-center>
+				<v-flex class="mt-5">
+					<v-avatar size="85">
+						<v-img src="/avatar-1.png"></v-img>
+					</v-avatar>
+					<p class="white--text subheading mt-1">The Net Ninja</p>
+				</v-flex>
+				<v-flex class="mt-4 mb-3">
+					<Popup />
+				</v-flex>
+			</v-layout>
+			<v-list>
+				<v-list-item
+					v-for="link in links"
+					:key="link.text"
+					router
+					:to="link.route"
+				>
+					<v-list-item-action>
+						<v-icon class="white--text">{{ link.icon }}</v-icon>
+					</v-list-item-action>
+					<v-list-item-content>
+						<v-list-item-title id="link-active" class="white--text">{{
+							link.text
+						}}</v-list-item-title>
+					</v-list-item-content>
+				</v-list-item>
+			</v-list>
+		</v-navigation-drawer>
+	</nav>
 </template>
 
 <script>
+import Popup from "./popup";
+
 export default {
-  data() {
-    return {
-      drawer: false,
-      links: [
-        { icon: "mdi-view-dashboard-variant", text: "Dashboard", route: "/" },
-        { icon: "mdi-folder-account", text: "My Projects", route: "/projects" },
-        { icon: "mdi-account-group", text: "Team", route: "/team" }
-      ]
-    };
-  }
+	components: {
+		Popup
+	},
+	data() {
+		return {
+			drawer: false,
+			links: [
+				{ icon: "mdi-view-dashboard-variant", text: "Dashboard", route: "/" },
+				{ icon: "mdi-folder-account", text: "My Projects", route: "/projects" },
+				{ icon: "mdi-account-group", text: "Team", route: "/team" }
+			]
+		};
+	}
 };
 </script>
 
 <style scoped>
 .v-list-item--active {
-  color: darkviolet !important;
+	color: darkviolet !important;
 }
 </style>
---
+++ src/components/popup.vue
@@ -0,0 +1,78 @@
+<template>
+	<div class="text-center">
+		<v-dialog max-width="600px">
+			<template v-slot:activator="{ on }">
+				<v-btn tile text v-on="on" class="success">
+					Add new project
+				</v-btn>
+			</template>
+			<v-card>
+				<v-card-title>
+					<h3>
+						Add a New Project
+					</h3>
+				</v-card-title>
+				<v-card-text>
+					<v-form class="px-3">
+						<v-text-field
+							color="purple accent-4"
+							label="Title"
+							v-model="title"
+							prepend-icon="mdi-folder"
+						></v-text-field>
+						<v-textarea
+							color="purple accent-4"
+							label="Information"
+							v-model="content"
+							prepend-icon="mdi-pencil"
+						></v-textarea>
+
+						<v-menu>
+							<template v-slot:activator="{ on }">
+								<v-text-field
+									color="purple accent-4"
+									label="Due date"
+									prepend-icon="mdi-calendar-range"
+									v-on="on"
+									:value="formattedDate"
+								>
+								</v-text-field>
+							</template>
+							<v-date-picker v-model="due"></v-date-picker>
+						</v-menu>
+
+						<v-btn text tile class="success" @click="submit">
+							Add Project</v-btn
+						>
+					</v-form>
+				</v-card-text>
+			</v-card>
+		</v-dialog>
+	</div>
+</template>
+
+<script>
+import format from "date-fns/format";
+import parseISO from "date-fns/parseISO";
+export default {
+	data() {
+		return {
+			title: "",
+			content: "",
+			due: null
+		};
+	},
+	methods: {
+		submit() {
+			console.log(this.title, this.content);
+		}
+	},
+	computed: {
+		formattedDate() {
+			return this.due ? format(parseISO(this.due), "do MMM yyyy") : "";
+		}
+	}
+};
+</script>
+
+<style scoped></style>
